ec63fa0657346fa19b1ac0d7abec4a269e01a581
Vaughan, Thomas E <tevaugha@ball.com>
2022-02-19 21:08:06 -0700

    Write explicit mainpage for Doxygen re #8.

---
+++ include/gslcpp/doc.hpp
@@ -0,0 +1,13 @@
+/// @file       include/gslcpp/doc.hpp
+/// @brief      Documentation for main page in output of Doxygen.
+/// @copyright  2022 Thomas E. Vaughan, all rights reserved.
+
+/// @mainpage
+///
+/// `gslcpp` implements a modern-C++ wrapper around a subset of GSL and in
+/// interface between Eigen and some of GSL's infrastructure for linear
+/// algebra.
+///
+/// See also the [repository at GitHub](https://github.com/tevaughan/gslcpp).
+
+// EOF
